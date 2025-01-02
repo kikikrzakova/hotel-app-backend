efc537ee6f7689d2287c4bcc83c15d27fbce63e6
Dieu Trang <krzakova.kristyna@gmail.com>
2025-01-02 20:19:37 +0100

    reservation system

--- app.js
+++ app.js
@@ -65,6 +65,7 @@ app.get("/booking/", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}` );
 });
