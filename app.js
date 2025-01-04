142c41c9db0d1cde399f4d9d43577df56284f4d1
Dieu Trang <krzakova.kristyna@gmail.com>
2025-01-04 16:43:10 +0100

    the function now successfully returns a single room that can fit the required amount of people

--- app.js
+++ app.js
@@ -59,15 +59,53 @@ app.get("/booking/", async (req, res) => {
     const bookedRooms = [
       ...new Set(overlappingBookings.map((booking) => booking.room)),
     ];
-    console.log("booked rooms: ", bookedRooms, `${bookedRooms ? typeof bookedRooms[0]: ""}`)
-    console.log("Room: ", Room)
+    // console.log("booked rooms: ", bookedRooms, `${bookedRooms ? typeof bookedRooms[0]: ""}`)
+    // console.log("Room: ", Room)
     // find all rooms that are not booked during the required dates
     const availableRooms = await Room.find(
       {number : {$nin: [...bookedRooms]}}
     )
-    console.log(availableRooms);
+    if (!availableRooms){
+      return res.status(204).json({
+        status: "fail",
+        data: null,
+      })
+    }
+    // console.log(availableRooms);
 
-    res.status(200).json({ status: "success", data: { availableRooms } });
+    // find all single rooms that satisfy the required capacity and are available
+    const availableSingleRooms = availableRooms.filter((room) => room.capacity >= guests)
+  
+    if (availableSingleRooms){
+      const bestRoom = availableSingleRooms.reduce((minRoom, room)=> {
+        if (room.capacity < minRoom.capacity){
+          console.log(room)
+          return room
+        } else return minRoom
+      }, availableSingleRooms[0])
+      return res.status(200).json({ status: "success", data: { bestRoom } });
+    }
+    // if (!availableSingleRooms) {
+    //   const sortedAvailableRooms = availableRooms.sort((roomA,roomB)=>roomB.capacity - roomA.capacity);
+    //   let totalCapacity = 0;
+    //   roomCombination = []
+    //   for (const room of sortedAvailableRooms) {
+    //     if (totalCapacity >= guests){
+    //       break;
+    //     }
+    //     totalCapacity += room.capacity;
+    //     roomCombination.push(room)
+    //   }
+    //   if (totalCapacity < guests ){
+    //     return res.status(204).json({
+    //       status: "fail",
+    //       message: "Not enough capacity for the number of guests",
+    //     })
+      
+    //   }
+    // }
+    
+    res.status(200).json({ status: "success", data: { availableSingleRooms } });
   } catch (err) {
     console.error(err);
     res.status(500).send("Server error");
