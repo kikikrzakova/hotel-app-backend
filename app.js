9bc8b30b58b0efea98d28593738e70bc8bb7a513
Dieu Trang <krzakova.kristyna@gmail.com>
2025-01-19 09:30:05 +0100

    updated

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const {
   createBooking,
   updateBooking,
   deleteBooking,
+  getAvailableBookings,
   Booking
 } = require("./bookings");
 const { getAllRooms, createRoom, updateRoom, deleteRoom, Room } = require("./rooms");
@@ -44,79 +45,10 @@ roomRouter
 app.use("/bookings", bookingRouter);
 app.use("/rooms", roomRouter);
 
-app.get("/booking/", async (req, res) => {
-  const { startDate, endDate, guests } = req.query;
-  console.log(startDate, endDate, guests);
-  try {
-    const query = {
-      $and: [
-        { checkIn: { $lte: new Date(endDate).toISOString() } },
-        { checkOut: { $gte: new Date(startDate).toISOString() } },
-      ],
-    };
-    const overlappingBookings = await Booking.find(query);
-    // find bookings where the booked dates overlap with the required dates
-    const bookedRooms = [
-      ...new Set(overlappingBookings.map((booking) => booking.room)),
-    ];
-    // console.log("booked rooms: ", bookedRooms, `${bookedRooms ? typeof bookedRooms[0]: ""}`)
-    // console.log("Room: ", Room)
-    // find all rooms that are not booked during the required dates
-    const availableRooms = await Room.find(
-      {number : {$nin: [...bookedRooms]}}
-    )
-    if (!availableRooms){
-      return res.status(204).json({
-        status: "fail",
-        data: null,
-      })
-    }
-    // console.log(availableRooms);
+// app.get("/booking/", getAvailableBookings);
 
-    // find all single rooms that satisfy the required capacity and are available
-    const availableSingleRooms = availableRooms.filter((room) => room.capacity >= guests)
-  
-    
-    // if there are single available rooms that can fit the required number of guests, we'll find a room with min capacity
-    if (availableSingleRooms.length > 0){
-      const bestRoom = availableSingleRooms.reduce((minRoom, room)=> {
-        if (room.capacity < minRoom.capacity){
-          console.log(room)
-          return room
-        } else return minRoom
-      }, availableSingleRooms[0])
-      return res.status(200).json({ status: "success", data: { bestRoom } });
-    }
-    console.log("seems to be working fine until here")
+app.route("/booking").get(getAvailableBookings)
 
-    // if there are no single available rooms that can fit the required number of guests, we'll find a combination of rooms that can fit the required number of guests
-    // sort the available rooms in ascending order to find a combination with min capacity
-    const sortedAvailableRooms = availableRooms.sort((roomA,roomB)=>roomA.capacity - roomB.capacity);
-    let totalCapacity = 0;
-    const roomCombination = [];
-    for (const room of sortedAvailableRooms) {
-      totalCapacity += room.capacity;
-      console.log(room);
-      roomCombination.push(room);
-      if (totalCapacity >= guests){
-        break;
-      }
-    }
-    if (totalCapacity < guests ){
-      return res.status(204).json({
-        status: "fail",
-        message: "Not enough capacity for the number of guests",
-      })
-    
-    } 
-    return res.status(200).json({ status: "success", data: { roomCombination } });
-    
-    
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Server error");
-  }
-});
 
 
 const PORT = 3000;
